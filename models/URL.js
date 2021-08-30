6d57a058e0e8321fc0bf8ff58904ca50f36f0471
Akshit Bansal <akshit.bansal755@gmail.com>
2021-08-30 12:30:28 +0530

    Added URL validation

--- models/URL.js
+++ models/URL.js
@@ -1,18 +1,27 @@
 const mongoose = require('mongoose');
+const validUrl = require('valid-url');
+const AppError = require('../utils/AppError');
 
 const URLSchema = new mongoose.Schema({
     url: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
     },
     shortened: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     redirects: {
         type: Number,
         default: 0
     }
 });
+
+URLSchema.pre('validate', (next) => {
+    if(validUrl.isWebUri(this.url)) next();
+    else throw new AppError('Invalid URL', 400);
+})
 
 module.exports = mongoose.model('URL', URLSchema);
